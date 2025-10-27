c55300213e732c3c06b4caba7e2301f2b318cfe1
jsj487 <jsj487@naver.com>
2025-10-27 22:10:08 +0900

    fix: 캐릭터 태초 이력 버그 해결

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,16 +8,16 @@ export default defineConfig({
     open: true,
     proxy: {
       "/health": "http://localhost:3000",
+      "/search-character": "http://localhost:3000",
+      "/subscribe": "http://localhost:3000",
+      "/subscribe-name": "http://localhost:3000",
+      "/character/summary": "http://localhost:3000",
       "/events": {
         target: "http://localhost:3000",
         changeOrigin: true,
         proxyTimeout: 0,
         ws: false,
       },
-      "/subscribe": "http://localhost:3000",
-      "/api": "http://localhost:3000",
-      "/search-character": "http://localhost:3000",
-      "/character/summary": "http://localhost:3000",
     },
   },
 });
